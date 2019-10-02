2b6ba54a50212edb369a288f87a657c980f98f41
Gabriel <gabriel.xavier>
2019-10-02 17:20:10 -0300

    trying change query on doctor specialty

--- src/modules/doctor/functions/doctor-specialties.ts
+++ src/modules/doctor/functions/doctor-specialties.ts
@@ -1,14 +1,26 @@
+import { Specialty } from "./../../../entity/Specialty";
 import { Doctor } from "./../../../entity/Doctor";
 import { ForbiddenError } from "apollo-server";
 import { failedToFetch } from "../../../utils/messages";
 
 export const doctorSpecialties = async ({ id }: Doctor) => {
 	try {
-		const specialties = await Doctor.findOne({
-			where: { id },
-			relations: ["doctorSpecialties"]
+		const specialties = await Specialty.find({
+			where: {
+				doctor: {
+					id
+				}
+			}
 		});
-		return specialties!.doctorSpecialties;
+
+		// console.log("aaaaaaa", specialties);
+		// const specialties2 = await Doctor.findOne({
+		// 	where: { id },
+		// 	relations: ["doctorSpecialties"]
+		// });
+		// console.log(specialties2!.doctorSpecialties);
+
+		return specialties;
 	} catch (err) {
 		throw new ForbiddenError(failedToFetch);
 	}
