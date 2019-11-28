61bac699fd13a722081bd906f22d2a5a10477af6
Gabriel de Matos Xavier <gabriel.xavier@HOTBHZA003030.hotmart.local>
2019-11-28 11:56:39 -0300

    new script

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,9 @@ const startServer = async () => {
 
 	await createConnection();
 
-	server.listen().then(({ url }) => {
+	const port = process.env.PORT || 4000;
+
+	server.listen({ port }).then(({ url }) => {
 		console.log(`🚀  Server ready at ${url}`);
 	});
 };
